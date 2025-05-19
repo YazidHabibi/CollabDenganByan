c29bc5f46bf77ebdce679f8f2c378bb8fa709de0
YazidHabibi <yazid2505@student.abudzar.sch.id>
2025-05-19 10:33:47 +0700

    test

--- collab-project/src/App.jsx
+++ collab-project/src/App.jsx
@@ -4,8 +4,8 @@ export default function App() {
         <h1 className="text-2xl text-center">
           ini halaman 
         </h1>
-      <div className="border-2 border-black w-22">
-        <div className="justify-center items-center"> 
+      <div className="border-2 border-black w-22 justify-center items-center">
+        <div className="flex  "> 
           <h2>Ini project react nama-antum</h2>
           <img src="hiu.jpg" alt="" />
         </div>
