f2e6a41ce2b43fee7ae578afdfebe126e31637b4
Life-In-Rio <151702664+Life-In-Rio@users.noreply.github.com>
2025-05-19 10:51:51 +0700

    kolabmas

--- collab-project/src/App.jsx
+++ collab-project/src/App.jsx
@@ -4,6 +4,11 @@ export default function App() {
 
       <h1 className="text-3xl font-bold my-5">Ini project react nama-nama antum 👍</h1>
       <img src="hiu.jpg" alt="" className="border-2 border-blue-500 rounded w-1/4"/>
+    
+
+
+      <h1 className="text-3xl font-bold my-5">Ini project react Mas bymeg 👍</h1>
+      <img src="/public/doa ibu.jpg" alt="" className="border-2 border-blue-500 rounded w-1/4"/>
     </div>
   );
 }
